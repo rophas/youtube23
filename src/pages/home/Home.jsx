6de4125ec147457ef34b348d320c86f1e4330071
rophas <rophasedward@gmail.com>
2025-04-22 13:22:51 +0300

    inked

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -17,7 +17,76 @@ const Home = () => {
         ))}
       </Slide>
       <div className="features">
-        <div className="container"></div>
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of freelance talent at your fingertip</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <video src="./img/video.mp4" controls></video>
+          </div>
+        </div>
+      </div>
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>Inked Business</h1>
+            <h1>A business solution designed for teams</h1>
+            <p>
+              Upgrade to a curated experience packed with tools and benefits, dedicated to businesses
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Get matched with the perfect talent by a customer success manager
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Manage teamwork and boost productivity with one powerful workspace
+            </div>
+            <button>Explore Inked Business</button>
+          </div>
+          <div className="item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lSbY5vGIeWmmluzeeEZZcjEh6NSPP5LCAA&s" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   )
