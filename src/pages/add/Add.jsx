c47ae92cd71f7026c1fa10392cccb618a4260e02
rophas <rophasedward@gmail.com>
2025-05-10 21:48:18 +0300

    alo

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -3,7 +3,31 @@ import "./Add.scss"
 
 const Add = () => {
   return (
-    <div className='add'>Add</div>
+    <div className='add'>
+      <div className="container">
+        <h1>Add New Gig</h1>
+        <div className="sections">
+          <div className="left">
+            <label htmlFor="">Title</label>
+            <input type="text" placeholder="e.g. I will do something I'm really good at" />
+            <label htmlFor="">Category</label>
+            <select name="cats" id="cats">
+              <option value="design">Design</option>
+              <option value="web">Web Development</option>
+              <option value="animation">Animation</option>
+              <option value="music">Music</option>
+            </select>
+            <label htmlFor="">Cover Image</label>
+            <input type="file" />
+            <label htmlFor="">Upload Images</label>
+            <input type="file" multiple />
+            <label htmlFor="">Description</label>
+            <textarea name="" id="" cols="30" rows="16"></textarea>
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
