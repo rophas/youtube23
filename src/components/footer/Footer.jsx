e82173e9f23ece09f27a1a76bb3cc189ca665f3f
rophas <rophasedward@gmail.com>
2025-05-24 11:23:10 +0300

    server

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -34,8 +34,8 @@ function Footer() {
             <h2>Support</h2>
             <span>Help & Support</span>
             <span>Trust & Safety</span>
-            <span>Selling on Liverr</span>
-            <span>Buying on Liverr</span>
+            <span>Selling on Inked</span>
+            <span>Buying on Inked</span>
           </div>
           <div className="item">
             <h2>Community</h2>
@@ -53,14 +53,14 @@ function Footer() {
           </div>
           <div className="item">
             <h2>More From Fiverr</h2>
-            <span>Liverr Business</span>
-            <span>Liverr Pro</span>
-            <span>Liverr Logo Maker</span>
-            <span>Liverr Guides</span>
+            <span>Inked Business</span>
+            <span>Inked Pro</span>
+            <span>Inked Logo Maker</span>
+            <span>Inked Guides</span>
             <span>Get Inspired</span>
-            <span>Liverr Select</span>
+            <span>Inked Select</span>
             <span>ClearVoice</span>
-            <span>Liverr Workspace</span>
+            <span>Inked Workspace</span>
             <span>Learn</span>
             <span>Working Not Working</span>
           </div>
@@ -68,8 +68,8 @@ function Footer() {
         <hr />
         <div className="bottom">
           <div className="left">
-            <h2>liverr</h2>
-            <span>© Liverr International Ltd. 2023</span>
+            <h2>Inked</h2>
+            <span>© Rophas International Ltd. 2025</span>
           </div>
           <div className="right">
             <div className="social">
